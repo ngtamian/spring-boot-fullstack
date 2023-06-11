c93328f09415e2279537bb475b0c9a00993e92b2
ngtamian <ngtamian@gmail.com>
2023-06-11 14:48:55 +0200

    Workflow CD with PingPongController

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -3,16 +3,13 @@ package com.amigoscode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-
 @RestController
 public class PingPongController{
 
     record PingPong(String result){}
 
     @GetMapping("/ping")
-    public List<Customer> getPingPong() {
+    public PingPong getPingPong() {
 
         return new PingPong("Pong");
     }
