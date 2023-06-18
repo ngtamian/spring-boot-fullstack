e935e9d54d2b778613e9af4635f06e91b6516c3b
ngtamian <ngtamian@gmail.com>
2023-06-18 21:07:47 +0200

    CORS ,AXIOS, Fetch Customers and Card Component

---
+++ frontend/react/src/App.tsx
@@ -0,0 +1,66 @@
+import {Wrap,
+       WrapItem,
+       Button,
+       Spinner, ButtonGroup, Text} from '@chakra-ui/react';
+import  SidebarWithHeader from "./components/shared/Sidebar.jsx"
+import React, {useEffect, useState} from "react";
+import { getCustomers} from './services/client.js';
+import  CardWithImage from "./components/Card.jsx"
+
+
+const  App = ()=> {
+
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(()=>{
+        setLoading(true);
+        getCustomers().then(res => {
+            setCustomers(res.data);
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            setLoading(false);
+        })
+
+    },[])
+    if(loading){
+        //return <Button colorScheme='teal' variant='outline'>Click me</Button>
+        return (<SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+        )
+    }
+    if(customers.length <=0){
+        return (<SidebarWithHeader>
+               <Text>No customers available</Text>
+            </SidebarWithHeader>
+        )
+
+    }
+
+    return (<SidebarWithHeader>
+            <Wrap justify={"center"} spacing={"30px"}>
+                {customers.map((customer,index)=>(
+                    <WrapItem key={index}>
+                        <CardWithImage
+                            id={customer.id}
+                            name={customer.name}
+                            age={customer.age}
+                            email={customer.email}
+                        />
+                    </WrapItem>
+                ))}
+            </Wrap>
+
+        </SidebarWithHeader>
+    )
+
+}
+
+export default  App;
